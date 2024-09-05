8f69833550f863c187f4f2a0c25a49e472e74954
pilot1784 <pilot1782@pilot1782.org>
2024-09-05 14:01:44 -0600

    init

---
+++ DevNet-1/Program.cs
@@ -0,0 +1,8 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+IWebDriver driver = new ChromeDriver();
+
+driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
+
+driver.Navigate().GoToUrl("https://www.selenium.dev/selenium/web/web-form.html");
