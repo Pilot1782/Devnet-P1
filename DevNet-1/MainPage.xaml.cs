98cab45a23af797b186df31578739c318425e077
Computercodes101 <computercodes101@gmail.com>
2024-09-10 16:06:58 -0600

    Add temporary input and output system

--- DevNet-1/MainPage.xaml.cs
+++ DevNet-1/MainPage.xaml.cs
@@ -5,22 +5,21 @@ namespace Devnet_1
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
-
-            Task.Run(RunScraper);
         }
 
-        private void RunScraper()
+        private async void RunScraper(String addr)
         {
             var scrap = new Scraper.Scraper();
 
+            await MainThread.InvokeOnMainThreadAsync(() =>
+            {
+                ProgressLabel.Progress = 0.33;
+            });
             Debug.WriteLine("Starting scraping...");
 
-            var addr = "18500 Murdock Circle";
             var pid = scrap.GetPiD(addr);
 
             if (pid != "notfound")
@@ -29,24 +28,47 @@ namespace Devnet_1
             }
             else
             {
+                await MainThread.InvokeOnMainThreadAsync(() =>
+                {
+                    ErrorLabel.Text = "Address not found.";
+                });
                 Debug.WriteLine("Failed to locate property with address: " + addr);
                 return;
             }
 
+            await MainThread.InvokeOnMainThreadAsync(() =>
+            {
+                ProgressLabel.Progress = 0.66;
+            });
+
             var keyData = scrap.GetKeyData(pid);
             Debug.WriteLine(JsonConvert.SerializeObject(keyData));
+
+            await MainThread.InvokeOnMainThreadAsync(() =>
+            {
+                ProgressLabel.Progress = 1;
+
+                OwnerLabel.Text = $"Owner: {keyData["owner"]}";
+                AddressLabel.Text = $"Address: {keyData["address"]}, {keyData["city"]}";
+                STRLabel.Text = $"Section: {keyData["section"]}, Township: {keyData["township"]}, Range: {keyData["range"]}";
+                LegalLabel.Text = $"Legal Description: {keyData["legal"]}";
+            });
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void OnRunClicked(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            if (AddressInput.Text != "")
+            {
+                ProgressLabel.IsVisible = true;
+                ProgressLabel.Progress = 0;
+                Task.Run(() =>
+                {
+                    RunScraper(AddressInput.Text);
+                });
+            } else
+            {
+                ErrorLabel.Text = "No address given.";
+            }
         }
     }
 
